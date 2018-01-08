42f06dedd41b8036038a1ef9f2561ba0ae242340
vint-eastwood <33958509+vint-eastwood@users.noreply.github.com>
2018-01-08 17:35:54 +0100

    Add files via upload

---
+++ Scripts/BearControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float speed = 20f;
+    public float jumpPower = 40f;
+
+    public bool grounded;
+
+    private Rigidbody2D rb2d;
+
+
+	void Start () {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void FixedUpdate ()
+    {
+
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce((Vector2.right * speed) * h);
+
+	}
+}
---
+++ Scripts/Groundcheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Groundcheck : MonoBehaviour
+{
+    private BearController player;
+
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<BearController>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        player.grounded = true;
+    }
+
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        player.grounded = false;
+    }
+
+
+
+}
